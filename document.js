c79a1aafb318fbcfc0cb90721ea8f054777a8d18
Ruban Dzmitry <ruban251219872023@gmail.com>
2023-11-23 13:28:27 +0300

    Alina

--- document.js
+++ document.js
@@ -1,13 +1,8 @@
-// let result = prompt('Сколько Вам лет?', '');
-// alert(`Тебе ${result} лет!`)
+let alinaRuban1 = prompt('Алина, привет! Ответь на пару вопросов... тут надо писать!', '');
+alert(`Спасибо за ${alinaRuban1}`);
+let alinaRuban2 = prompt('Как дела?', '');
+alert(`И у меня ${alinaRuban2}`);
+let alinaRuban3 = prompt('Как тебе сегодня погода?', '');
+alert(`Действительно ${alinaRuban3}`);
+alert('Спасибо за внимание! Тебе подарок Pac - Man)))');
 
-
-let rubanOlga3 = prompt('Будет время, ответь на пару вопросов, надо писать!!!', '');
-alert(`Спасибо, что ${rubanOlga3}`);
-let rubanOlga = prompt('Ты меня любишь?', '');
-alert(`Я так и знал, что ${rubanOlga}`);
-let rubanOlga2 = prompt('Как тебе погода, сегодня?', '');
-alert(`Действительно, ${rubanOlga2}`);
-let rubanOlga4 = prompt('Бонуса нету, не хотят платить, напиши что-нибудь...', '');
-alert(`Ты написала вот это... ${rubanOlga4}, я угадал!`);
-alert('Спасибо, больше нет вопросов!');
